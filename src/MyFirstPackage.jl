cac42dad3b0af24a01ee9374c7057b0bcb2b533b
HuoXiaoGuang <xhuo740@connect.hkust-gz.edu.cn>
2024-03-13 20:46:58 +0800

    initial

--- src/MyFirstPackage.jl
+++ src/MyFirstPackage.jl
@@ -2,4 +2,16 @@ module MyFirstPackage
 
 # Write your package code here.
 
+# import packages
+using LinearAlgebra
+
+# export interfaces
+export Lorenz, integrate_step
+export Point, Point2D, Point3D
+export RungeKutta, Euclidean
+
+# `include` other source files into this module
+include("lorenz.jl")
+
+
 end
